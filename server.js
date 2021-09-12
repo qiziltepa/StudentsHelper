0b2b57d5157747b6a51489ff20b874380dd8b0a0
Developer0772 <alishernarzullayev7777@gmail.com>
2021-09-12 12:29:42 +0500

    backend

---
+++ server.js
@@ -0,0 +1,16 @@
+const express =  require('express')
+const app = express();
+
+
+app.use(express.static('public'))
+app.use(express.static('public'))
+var PORT = 3000;
+
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html')
+})
+
+app.listen(PORT,()=>{
+    console.log("localhost:5500");
+})
